c344bb62617860302c706d41bdd0bd84c8431aef
kkosinsk <kkosinsk@umich.edu>
2014-10-15 15:39:30 -0400

    Set up basic activities-Kathy

--- app/src/main/java/com/example/ski/spot/AddLink.java
+++ app/src/main/java/com/example/ski/spot/AddLink.java
@@ -6,23 +6,26 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
 import android.content.Intent;
 
 
 
-public class AddActivity extends Activity {
+
+public class AddLink extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add);
+        setContentView(R.layout.activity_add_link);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.add, menu);
+        getMenuInflater().inflate(R.menu.add_link, menu);
         return true;
     }
 
@@ -37,28 +40,7 @@ public class AddActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
-    public void addSound(View view) {
-        Intent intent= new Intent(this, AddSound.class);
-        startActivity(intent);
-    }
-    public void addPhoto(View view) {
-        Intent intent= new Intent(this, AddPhoto.class);
-        startActivity(intent);
-    }
-    public void addLink(View view) {
-        Intent intent= new Intent(this, AddLink.class);
-        startActivity(intent);
-    }
-    public void addText(View view) {
-        Intent intent = new Intent(this, AddText.class);
-        startActivity(intent);
-    }
-    public void addQuote(View view) {
-        Intent intent= new Intent(this, AddQuote.class);
-        startActivity(intent);
-    }
-    public void addMovie(View view) {
-        Intent intent= new Intent(this, AddVideo.class);
-        startActivity(intent);
+    public void submitLink(View view) {
+        //submit the link to the next page
     }
 }
