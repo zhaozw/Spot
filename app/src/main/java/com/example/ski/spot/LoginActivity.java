68989fdf39e7610b0b5936a164c6f93464557cb4
sylzhang <sylzhang@umich.edu>
2014-10-16 11:36:43 -0400

    login java

--- app/src/main/java/com/example/ski/spot/LoginActivity.java
+++ app/src/main/java/com/example/ski/spot/LoginActivity.java
@@ -1,12 +1,15 @@
 package com.example.ski.spot;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class LoginActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,23 +17,9 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.activity_login);
     }
 
+    public void logIn(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
