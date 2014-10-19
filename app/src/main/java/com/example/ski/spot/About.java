96b1c06ba259dd2703a72954888c9e1023a30c06
kkosinsk <kkosinsk@umich.edu>
2014-10-19 15:21:10 -0400

    About activity

--- app/src/main/java/com/example/ski/spot/About.java
+++ app/src/main/java/com/example/ski/spot/About.java
@@ -3,8 +3,10 @@ package com.example.ski.spot;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class About extends Activity {
@@ -13,6 +15,7 @@ public class About extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
     }
 
 
