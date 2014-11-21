449a4c4703f0eb095d7c413e8baa2e13d0a76e2a
kkosinsk <kkosinsk@umich.edu>
2014-11-21 00:09:29 -0500

    Looking at Teamivore adapter code
    and seeing how it will combine and can mesh with ours.

---
+++ app/src/main/java/com/example/ski/spot/MomentDetailActivity.java
@@ -0,0 +1,134 @@
+package com.example.ski.spot;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MomentDetailActivity extends Activity {
+
+    List<Moment> momentsList;
+
+    int momentId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moment_detail);
+
+        initList();
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(OverviewActivity.EXTRA_MESSAGE);
+
+        momentId = (int) Long.parseLong(message);
+
+        // Create the text view
+        TextView textView = (TextView) findViewById(R.id.textView_momentname);
+        textView.setText(momentsList.get(momentId).getName());
+
+        // Just to demonstrate, the following two lines grab a rating bar element and set its value
+        // programatically
+        RatingBar ratings = (RatingBar) findViewById(R.id.ratingBar2);
+        ratings.setRating(2);
+
+    }
+
+    // initList is a helper method that grabs our "team data" and associates it with the correct id
+    // based on its position in the list
+    // in a real application, this would be where we query the database and fetch the data associated
+    // with the teams
+    // we can perform some shared preferences data storing for now
+    private void initList() {
+
+        momentsList = SharedPreferencesUtility.getMomentList(this, "moments");
+
+    }
+
+    // onCreateOptionsMenu adds the options menu to your activity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.moment_detail, menu);
+        return true;
+    }
+
+    public void deleteMomentDialog() {
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MomentDetailActivity.this);
+
+        // Setting Dialog Title
+        alertDialog.setTitle("Confirm Delete...");
+
+        // Setting Dialog Message
+        alertDialog.setMessage("Are you sure you want delete this moment?");
+
+        // Setting Icon to Dialog
+        alertDialog.setIcon(android.R.drawable.ic_dialog_info);
+
+        // Setting Positive "Yes" Button
+        alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int which) {
+                // Write your code here to invoke YES event
+                deleteMoment();
+            }
+        });
+
+        // Setting Negative "NO" Button
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        // Showing Alert Message
+        alertDialog.show();
+
+    }
+
+    public void deleteMoment() {
+
+        momentsList.remove(momentId);
+        SharedPreferencesUtility.putMomentList(this, "moments", momentsList);
+        Toast.makeText(getApplicationContext(), "Moment has been deleted!", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this, OverviewActivity.class);
+        startActivity(intent);
+
+        finish();
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            // launch intent to settings screen
+            return true;
+        }
+        else if (id == R.id.action_logout) {
+            // launch intent to settings screen
+            return true;
+        }
+        else if (id == R.id.action_delete_moment) {
+            deleteMomentDialog();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/ski/spot/OverviewActivity.java
@@ -0,0 +1,199 @@
+package com.example.ski.spot;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class OverviewActivity extends Activity {
+
+    // the string variable we use for sending messages with intents
+    public final static String EXTRA_MESSAGE = "com.example.ski.spot";
+
+    // a list class type must be used when using a list view
+    // list items are added to a list view programatically and not through xml
+    List<Map<String, String>> momentsList = new ArrayList<Map<String,String>>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overview);
+
+        initData();
+
+        // we need to pick which view we want our context menu to respond to
+        // here, we've chosen the list of team names (which in turn applies the context menu to
+        // each list item separately)
+        registerForContextMenu((ListView) findViewById(R.id.listView));
+
+        // we call this initiList function to fill in our list class variable with our team names
+        initList();
+
+        // adapters are what we use to associate the list variable and its contents with the list view
+        ListView momentListView = (ListView) findViewById(R.id.listView);
+        SimpleAdapter simpleAdpt = new SimpleAdapter(this, momentsList, android.R.layout.simple_list_item_1, new String[] {"team"}, new int[] {android.R.id.text1});
+        momentListView.setAdapter(simpleAdpt);
+
+        // setOnItemClickListener tells the activity what to do when a list item is clicked on
+        momentListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parentAdapter, View view, int position,
+                                    long id) {
+                openMomentDetail(id);
+            }
+        });
+
+    }
+
+    // initList simply adds our team names to the list variable
+    // in a real app, this would be where we query our database to retrieve the list of teams, but
+    // we can perform some shared preferences data storing for now
+    private void initList() {
+
+        List<Moment> moments = SharedPreferencesUtility.getMomentList(this, "moments");
+
+        for(Moment t: moments) {
+
+            momentsList.add(createMoment("moment", t));
+
+        }
+
+    }
+
+    private void initData() {
+
+        /*List<String> teamsList = new ArrayList<String>();
+        teamsList.add("Team 1: Uproar Labs");
+        teamsList.add("Team 2: Bogus Trust");
+        teamsList.add("Team 3: Gejago");
+        teamsList.add("Team 4: Jagurr");
+        teamsList.add("Team 5: Raspy Puck");
+        teamsList.add("Team 6: Ladybug Studios");
+        teamsList.add("Team 7: Fourtwelve");
+        SharedPreferencesUtility.putStringList(this, "teams", teamsList);
+        */
+
+        List<Moment> momentsList = new ArrayList<Moment>();
+
+        String defaultDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" +
+                " eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam," +
+                " quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+                " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu " +
+                "fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in " +
+                "culpa qui officia deserunt mollit anim id est laborum.";
+
+        momentsList.add(new Moment("Uproar Labs", defaultDescription));
+        momentsList.add(new Moment("Bogus Trust", defaultDescription));
+        momentsList.add(new Moment("Gejago", defaultDescription));
+        momentsList.add(new Moment("Jagurr", defaultDescription));
+        momentsList.add(new Moment("Raspy Puck", defaultDescription));
+        momentsList.add(new Moment("Ladybug Studios", defaultDescription));
+        momentsList.add(new Moment("Fourtwelve", defaultDescription));
+
+        SharedPreferencesUtility.putMomentList(this, "moments", momentsList);
+
+    }
+
+    // this method helps us minimize the amount of repeat calls we need to make in initList to place
+    // a team name into out list
+    private HashMap<String, String> createMoment(String key, Moment t) {
+        HashMap<String, String> moment = new HashMap<String, String>();
+        String name = t.getName();
+        moment.put(key, name);
+        return moment;
+    }
+
+    // openTeamDetail is called whenever a list item is clicked on
+    // it calls for an intent that starts up the team detail activity and sends the team's id over
+    // to the activity with the message variable declared at the top of the activity
+    public void openMomentDetail(long id) {
+        Intent intent = new Intent(this, MomentDetailActivity.class);
+        String message = String.valueOf(id);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    // onCreateOptionsMenu adds the options menu to your activity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.overview, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            // launch intent to go to settings screen
+            return true;
+        }
+        else if (id == R.id.action_logout) {
+            // launch intent to log out the user (that then sends user back to login activity)
+            return true;
+        }
+        else if (id == R.id.action_add_moment) {
+            addMoment();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    // onCreateContextMenu associates the context menu found in the menu resources folder with this
+    // activity
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.context_menu, menu);
+    }
+
+    // onContextItemSelected tells the activity what to do when a menu item in the context menu is
+    // clicked on
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        //AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_favorite) {
+            // message that this one is in favorites
+            // proceed to add to favorites
+            return true;
+        }
+        else if(id == R.id.action_contact) {
+            // initiate an intent to send user to contact activity with this team's email as the
+            // data
+            return true;
+        }
+
+        return super.onContextItemSelected(item);
+
+    }
+
+    public void addMoment() {
+        Intent intent = new Intent(this, AddMomentActivity.class);
+        startActivity(intent);
+    }
+
+}
